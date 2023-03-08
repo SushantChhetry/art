35c3eba91ce147fe656b3aa6a4c322b968a900af
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-03-07 22:10:54 -0600

    fixed fetch and added additional states

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,47 @@ import styles from "@/styles/Home.module.css";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const url = "https://api.artic.edu/api/v1/artworks/search?q=cats";
+  const url =
+    "https://api.artic.edu/api/v1/artworks/search?q=abstract&fields=id,title,image_id";
 
-  const [paintings, setPaintings] = useState();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [paintings, setPaintings] = useState({});
 
+  //Add objects to paintings with the img address and title using the getImage function
+
+  const getImage = (value) => {
+    for (let i = 0; i < value.length; i++) {
+      console.log(value[i].image_id);
+    }
+  };
   useEffect(() => {
     fetch(url)
       .then((response) => {
-        return response.json();
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
       })
       .then((data) => {
-        setPaintings(data);
-        console.log(paintings);
+        setData(data);
+        getImage(data.data);
       })
       .catch((err) => {
-        console.log(err.message);
+        console.log(`Error fetching data ${err}`);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) return <p>Error!</p>;
 
   return (
     <>
@@ -38,6 +62,7 @@ export default function Home() {
           <input type="text" />
           <input type="submit" />
         </form>
+        {console.log(data.data)}
       </main>
     </>
   );
