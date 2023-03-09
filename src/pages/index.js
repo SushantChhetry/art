1d53418e5239f6c0f694633206584bb443246a1c
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-03-08 19:32:14 -0600

    Added basic CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="body-wrapper">
         <h1>Art.</h1>
         <p>Chicago Institute of Art</p>
         <form>
