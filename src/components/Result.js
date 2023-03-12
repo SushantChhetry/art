20ea2063fe95a85a88d4e1beeb50ad415a4e8ea7
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-03-12 18:14:58 -0500

    Initial images using api

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 
-const Result = ({ data }) => {
-  const [paintings, setPaintings] = useState(data);
+const Result = (props) => {
+  const [paintings, setPaintings] = useState([]);
 
   const [imgId, setImgId] = useState("");
+
+  useEffect(() => {}, []);
 
   //map thrpugh the data, extract the img id and put it in the src, extract the title, return them for rendering
 
@@ -11,7 +13,15 @@ const Result = ({ data }) => {
 
   return (
     <div>
-      {}
+      {console.log("paintings")}
+      {console.log(props.data ? props.data[0]["image_id"] : "")}
+      {props.data
+        ? props.data.map((val) => {
+            console.log(val["image_id"]);
+            const imgSrc = `https://www.artic.edu/iiif/2/${val["image_id"]}/full/843,/0/default.jpg`;
+            return <img src={imgSrc} alt="tests" />;
+          })
+        : ""}
       <div className="img-warpper">
         <img
           src="https://www.artic.edu/iiif/2/e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9/full/843,/0/default.jpg"
